63d2c24034c7680fe54d52588d53736a46e5e9a0
mylucien <53183558+mylucien@users.noreply.github.com>
2022-07-11 14:19:02 +0800

    Update generic.mk

--- target/linux/ipq60xx/image/generic.mk
+++ target/linux/ipq60xx/image/generic.mk
@@ -36,3 +36,37 @@ define Device/wf_hr6001
 	DEVICE_PACKAGES :=  uboot-envtools
 endef
 TARGET_DEVICES += wf_hr6001
+
+define Device/cmiot_ax18
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS := qcom-ipq6018-ax18
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	DEVICE_TITLE := CMIOT AX18
+endef
+TARGET_DEVICES += cmiot_ax18
+
+define Device/qihoo_v6
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS := qcom-ipq6018-qihoo-v6
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	DEVICE_TITLE := Qihoo V6
+	DEVICE_PACKAGES := kmod-usb3 kmod-usb-phy-msm
+endef
+TARGET_DEVICES += qihoo_v6
+
+define Device/zn_m2
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS := qcom-ipq6018-zn-m2
+	DEVICE_DTS_CONFIG := config@cp03-c1
+	DEVICE_TITLE := ZN M2
+endef
+TARGET_DEVICES += zn_m2
